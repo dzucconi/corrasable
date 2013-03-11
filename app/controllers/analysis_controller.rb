9a991362f779986a5af7a6df482863ebc4ae8d37
dzucconi <damonzucconi@gmail.com>
2013-03-10 22:30:37 -0400

    Removes unused actions

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1,26 +1,15 @@
 class AnalysisController < ApplicationController
   before_filter :get_report
 
-  def debug
-    render
-  end
-
   def report
     render "analysis/report"
-  end
-
-  def sentences
-    render json: {
-      length: @report.num_sentences,
-      sentences: @report.sentences
-    }
   end
 
   def syllables
     @syllables = @report.words.collect do |word|
       [word, Lingua::EN::Syllable.syllables(word)]
     end
-    
+
     render json: @syllables
   end
 
