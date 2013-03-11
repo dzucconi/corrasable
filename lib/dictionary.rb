e64a81c8279dfa752de17e7a5239456e57a5d94d
dzucconi <damonzucconi@gmail.com>
2013-03-10 22:30:18 -0400

    Class for parsing CMUdict data

---
+++ lib/dictionary.rb
@@ -0,0 +1,29 @@
+class Dictionary
+  DICTIONARIES = {
+    en: "#{Rails.root}/db/cmudict.0.7a.txt"
+  }
+  
+  def self.generate(language=:en)
+    IO.foreach(DICTIONARIES[language]).each do |line|
+      next if line !~ /^[A-Z]/
+      
+      line.chomp!
+
+      (word, *phonemes) = line.split(/  ?/)
+
+      # Ignore alternative pronunciations
+      # 
+      next if word =~ /\(\d\) ?$/
+      
+      syllables = phonemes.grep(/^[AEIUO]/).length
+      
+      create(word, phonemes, syllables, language)
+    end
+  end
+  
+  def self.create(word, phonemes, syllables, language=:en)
+    puts "#{word} #{phonemes} - #{syllables} (#{language})"
+
+    # Word.create!(word: word.upcase, phonemes: phonemes, syllables: syllables.to_i, language: language.to_s)
+  end
+end
