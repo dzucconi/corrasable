0bcf0cf16cdb74e1581118c29c35118cea64ef06
dzucconi <damonzucconi@gmail.com>
2013-03-11 22:21:10 -0400

    Convenience methods for looking up words

--- app/models/word.rb
+++ app/models/word.rb
@@ -7,4 +7,16 @@ class Word
   field :language, type: Symbol
 
   index({ word: 1 }, { name: "word_index" })
+
+  def self.lookup(word)
+    find_by(word: word.upcase)
+  end
+
+  def self.syllables(word)
+    lookup(word).syllables
+  rescue Mongoid::Errors::DocumentNotFound
+    # Fallback to guessing if the word is not in the dictionary
+    # 
+    Lingua::EN::Syllable.syllables(word)
+  end
 end
