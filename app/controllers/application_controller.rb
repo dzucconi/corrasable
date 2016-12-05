630f901ba2f1fa162b90d26724f988449e05c406
dzucconi <mail@damonzucconi.com>
2016-12-05 16:24:25 +0100

    Allows iframing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  after_action :allow_iframe
+
   def home
     @output = HTML.render(File.open("#{Rails.root}/README.md").read)
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
