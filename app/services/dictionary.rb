df000d3b725f87761e5b99f98c37314e7b0aa78d
dzucconi <mail@damonzucconi.com>
2017-01-31 22:29:07 +0100

    Silent option

--- app/services/dictionary.rb
+++ app/services/dictionary.rb
@@ -3,7 +3,7 @@ class Dictionary
     en: "#{Rails.root}/db/cmudict.0.7a.txt"
   }.freeze
 
-  def self.generate(language = :en)
+  def self.generate(language = :en, silent = false)
     IO.foreach(DICTIONARIES[language]).each do |line|
       next if line !~ /^[A-Z]/
 
@@ -17,12 +17,12 @@ class Dictionary
       # Count the number of stressed vowels
       syllables = phonemes.grep(/^[AEIUO]/).size
 
-      create(word, phonemes, syllables, language)
+      create(word, phonemes, syllables, language, silent)
     end
   end
 
-  def self.create(word, phonemes, syllables, language = :en)
-    puts "#{word} #{phonemes} - #{syllables} (#{language})"
+  def self.create(word, phonemes, syllables, language = :en, silent = false)
+    puts "#{word} #{phonemes} - #{syllables} (#{language})" unless silent
 
     Word.create!(word: word.upcase, phonemes: phonemes, syllables: syllables.to_i, language: language)
   end
